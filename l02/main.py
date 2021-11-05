f0cc816a0d5d85d0d85933486f478cc55c395d04
Sebastian Grigor <sgrigor@montran.com>
2021-11-05 18:40:13 +0200

    Add statement classes

--- l02/main.py
+++ l02/main.py
@@ -1,91 +1,147 @@
 
-import os 
+import os
 import itertools
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 indir = 'level2'
 output_file = os.path.join(dir_path, 'out')
 
-inputs = [os.path.join(dir_path, indir, indir + '_example.in')] + [os.path.join(dir_path, indir, indir + f'_{x}.in')  for x in range(1, 6)]
+inputs = [os.path.join(dir_path, indir, indir +
+                       f'_{x}.in') for x in range(1, 6)]
 
-outputs = [os.path.join(dir_path, output_file, indir + '_example.out')]+ [os.path.join(dir_path, output_file, indir + f'_{x}.out')  for x in range(1, 6)]
+outputs = [os.path.join(dir_path, output_file, indir +
+                        f'_{x}.out') for x in range(1, 6)]
 
 
+class Statement:
+    def exec(self):
+        raise Exception("DONT EXEC STATEMENTS")
 
-stack = []
+    def length():
+        raise Exception("DONT EXEC STATEMENTS")
 
-def handle_return(tokens, out, i):
-    raise ValueError('done job')
 
-def handle_print(tokens, out, i):
-    out.append(tokens[i+1])
-    return i + 2
+class Start(Statement):
+    def __init__(self, statements):
+        self.statements = statements
 
-def handle_if(tokens, out, i, variables):
-    next_ = tokens[i + 1]
-    condition = False
+    def exec(self):
+        for statement in self.statements:
+            statement.exec()
 
-    if next_ == 'true':
-        condition = True
-    i += 2
-    while True:
-        if tokens[i] == 'end':
-            i += 1
-            break
-        if condition:
-            i = handlers[tokens[i]](tokens, out, i, variables)
+    def length(self):
+        sum = 0
+
+        for statement in self.execstatements:
+            sum += statement.length()
+
+        return
+
+
+class Print(Statement):
+    def __init__(self, token, output):
+        self.token = token
+        self.output = output
+
+    def exec(self):
+        self.output += [self.token]
+
+    def length(self):
+        return 2
+
+
+class Return(Statement):
+    def __init__(self, token):
+        self.token = token
+
+    def exec(self):
+        raise Exception("DONE")
+
+    def length(self):
+        return 2
+
+
+class IfElse(Statement):
+    def __init__(self, condition, if_statements, else_statements):
+        self.condition = condition
+        self.if_statements = if_statements
+        self.else_statements = else_statements
+
+    def exec(self):
+        if self.condition == 'true':
+            for statement in self.if_statements:
+                statement.exec()
         else:
-            i += 1
-    if tokens[i] == 'else':
-        i += 1
-        while True:
-            if tokens[i] == 'end':
-                i += 1
-                break
-            if not condition:
-                i = handlers[tokens[i]](tokens, out, i, variables)
-            else:
-                i += 1
-    return i
+            for statement in self.else_statements:
+                statement.exec()
 
-handlers = {
-    'return': handle_return,
-    'print': handle_print,
-    'if': handle_if,
-}
+    def length(self):
+        sum = 0
+
+        for statement in self.if_statements:
+            sum += statement.length()
+
+        for statement in self.else_statements:
+            sum += statement.length()
+
+        return 5 + sum
+
+
+def parse_if_else(tokens, output):
+    if_statements = parse_statements(tokens[2:], output)
+
+    sum = 0
+    for statement in if_statements:
+        sum += statement.length()
+
+    else_statements = parse_statements(tokens[4 + sum:], output)
+    return IfElse(tokens[1], if_statements, else_statements)
+
+
+def parse_statements(tokens, output):
+    if tokens[0] == 'start':
+        return [Start(parse_statements(tokens[1:], output))]
+    elif tokens[0] == 'print':
+        return [Print(tokens[1], output)] + parse_statements(tokens[2:], output)
+    elif tokens[0] == 'if':
+        if_else_statement = parse_if_else(tokens, output)
+        return [if_else_statement] + parse_statements(tokens[if_else_statement.length():], output)
+    elif tokens[0] == 'return':
+        return [Return(tokens[1])] + parse_statements(tokens[2:], output)
+    elif tokens[0] == 'end':
+        return []
+    else:
+        raise Exception("SHIT", tokens)
+
 
 def main():
     print(inputs)
     print(outputs)
-
-
 
     for index, input_file in enumerate(inputs):
         out = []
 
         with open(input_file, 'r') as fd:
             lines = [line.strip() for line in fd.readlines()]
-            n = int(lines[0])
             lines = lines[1:]
             tokens = []
             for line in lines:
                 tokens.extend(line.split(' '))
-            
-            tokens = tokens[1:-1]
 
-            i = 0
-            while i < len(tokens):
-                token = tokens[i]
-                try:
-                    i = handlers[token](tokens, out, i)
-                except ValueError:
-                    break
+            startStatement = parse_statements(tokens, out)[0]
+
+            try:
+                startStatement.exec()
+            except:
+                pass
 
             out = ''.join(out)
 
             with open(outputs[index], 'w') as fdout:
                 fdout.write(out)
 
+            print("DONE")
+
+
 if __name__ == '__main__':
-    # print(inputs)
     main()
